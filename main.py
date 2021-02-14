de9fa23ad80fddf57b2208d35ec53f0b72d6b294
Jia Qi Poon <jqp18@ic.ac.uk>
2021-02-14 18:35:04 +0800

    Fix bugs in recognizer

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from recognizer import face_detection
+# from accel import accelerometer
+import time
 
+trainerfile = 'trainer/trainer.yml'
+cascadepath = 'Cascades/haarcascade_frontalface_default.xml'
+names = ['None', 'Siyu', 'JQ', 'Sherwin', 'Tianyi']
 
-cv = face_detection('trainer/trainer.yml', 
-                    "Cascades/haarcascade_frontalface_default.xml", )
+cv_instance = face_detection(trainerfile, cascadepath, names)
+# accel_instance = accelerometer()
+
+while True:
+    has_face, names = cv_instance.get_face()
+    print(has_face)
+    print(names)
+    time.sleep(0.5)
